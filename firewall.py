fe0e47aa8c20c264e05e68b54ac9ef6f8bbd64a8
Jerry <jzlung@gmail.com>
2013-11-08 00:12:47 -0800

    just need to fix up DNS parsing

--- firewall.py
+++ firewall.py
@@ -73,9 +73,10 @@ class Firewall:
 
         protocol = struct.unpack('!B', pkt[9:10])
         protocol = int(protocol[0])
-        print protocol
+        #print protocol
 
         if pkt_dir == PKT_DIR_INCOMING:
+            #print "INCOMING"
             source = struct.unpack('!L', pkt[12:16])
             source = source[0]
             if protocol == 17 or protocol == 6:
@@ -83,37 +84,109 @@ class Firewall:
                 srcport = srcport[0]
             if protocol == 1:
                 print "ICMP"
+                drop = self.handle_ICMP(source, srcport)
             elif protocol == 6:
-                print "TCP"
+                #print "TCP"
+                drop = self.handle_TCP(source, srcport)
             elif protocol == 17:
                 print "UDP"
                 drop = self.handle_UDP(source, srcport)
             print drop
             if drop == False:
                 self.iface_int.send_ip_packet(pkt)
-                print "incoming, send"
+                #print "incoming, send"
 
         else:
+            #print "OUTGOING"
             dest = struct.unpack('!L', pkt[16:20])
             dest = dest[0]
-        
             if protocol == 17 or protocol == 6:
                 destport = struct.unpack('!H', pkt[hlen+2:hlen+4])
                 destport = destport[0]
 
                 if protocol == 1:
                     print "ICMP"
+                    drop = self.handle_ICMP(dest, destport)
                 elif protocol == 6:
                     print "TCP"
+                    drop = self.handle_TCP(dest, destport)
                 elif protocol == 17:
                     print "UDP"
-                    drop = self.handle_UDP(dest, destport)
+                    #h2len = struct.unpack('!H', pkt[hlen+4:hlen+6])
+                    #print hlen
+                    #print h2len
+                    #h2len = int(h2len[0]) + hlen
+                    #print h2len
+                    h2len = hlen + 8
+                    if destport == 53:
+                        print "DNS FOR LIFE"
+                        drop = self.handle_DNS(h2len, pkt)
+                    else:
+                        drop = self.handle_UDP(dest, destport)
                 print drop
 
             if drop == False:
                 self.iface_ext.send_ip_packet(pkt)
-                print "outgoing, send"
+                #print "outgoing, send"
         
+    def decoder(self, s):
+        i = 0
+        sz = ""
+        while s[i] != chr(6):
+            i += 1
+        i = i+1
+        while s[i] != chr(3):
+            sz += s[i]
+            i += 1
+        sz += '.'
+        i += 1
+        while s[i] != chr(3) or s[i] != 0:
+            sz += s[i]
+            i += 1
+            if i == len(s):
+                break
+        return sz
+
+    def handle_DNS(self, h2len, pkt):
+        QDCount = struct.unpack('!H', pkt[h2len+4:h2len+6])
+        QDCount = int(QDCount[0])
+        if QDCount != 1:
+            return True
+        Qtype = struct.unpack('!H', pkt[-4:-2])[0]
+        if Qtype != 1 and Qtype != 28:
+            print "not A and not AAAA", Qtype
+            return True
+        Qclass = struct.unpack('!H', pkt[-2:])[0]
+        if Qclass != 1:
+            print "not internet"
+            return True
+        
+        print "inside handle DNS"
+        Qname = ""
+        ch = struct.unpack('!B', pkt[h2len+12:h2len+12+1])[0]
+        x = pkt[h2len+12:]
+        i = 1
+        while ch != 0:
+            print "ch", ch
+            if not(ch == 3 and i == 1) :
+                if(ch == 3 or ch == 6):
+                    Qname += '.'
+                else:
+                    Qname += chr(ch)
+            ch = struct.unpack('!B', pkt[h2len+12+i:h2len+12+1+i])[0]
+            i += 1
+        print "Qname:", Qname
+        split_domain = Qname.split('.')
+        domain = split_domain[-2] + '.' + split_domain[-1]
+        print "domain after split", domain
+
+        #domain = self.decoder(Qname)
+        #print "Domain:", self.decoder(Qname)
+
+        for rule in self.rules["dns"]:
+            if rule[2] == domain and rule[0] == "drop":
+                print "drop time"
+                return True
 
     def handle_UDP(self, extIP, port):
         drop = False
@@ -122,16 +195,16 @@ class Firewall:
             IP = self.ip_conv(extIP)
             ccn = self.geo_search(IP, self.geoipdb)
            # print ccn
-            if rule[2] == IP or rule[2] == 'any':
+            if rule[2] == IP or rule[2] == 'any' or rule[2] == ccn:
                 if rule[0] == 'drop': 
                     drop = True
-                    print "gonna drop 1"
+                    #print "gonna drop 1"
                 else:
                     drop = False
             if rule[3] == port or rule[2] == 'any':
                 if rule[0] == 'drop': 
                     drop = True
-                    print "gonna drop 2"
+                    #print "gonna drop 2"
                 else:
                     drop = False
         #print drop
@@ -142,46 +215,76 @@ class Firewall:
         for rule in self.rules["tcp"]:
             #print rule
             IP = self.ip_conv(extIP)
-            ccn = self.geo_search(extIP, self.geoipdb)
-            #print ccn
-            if rule[2] == IP or rule[2] == 'any':
+            ccn = self.geo_search(IP, self.geoipdb)
+           # print ccn
+            if rule[2] == IP or rule[2] == 'any' or rule[2] == ccn:
+                if rule[0] == 'drop': 
+                    drop = True
+                    #print "gonna drop 1"
+                else:
+                    drop = False
+            if rule[3] == port or rule[2] == 'any':
+                if rule[0] == 'drop': 
+                    drop = True
+                    #print "gonna drop 2"
+                else:
+                    drop = False
+        #print drop
+        return drop
+
+    def handle_ICMP(self, extIP, type):
+        drop = False
+        for rule in self.rules["icmp"]:
+            #print rule
+            IP = self.ip_conv(extIP)
+            ccn = self.geo_search(IP, self.geoipdb)
+           # print ccn
+            if rule[2] == IP or rule[2] == 'any' or rule[2] == ccn:
                 if rule[0] == 'drop': 
                     drop = True
                     print "gonna drop 1"
                 else:
                     drop = False
-            if rule[3] == port or rule[2] == 'any':
+            if rule[3] == type or rule[2] == 'any':
                 if rule[0] == 'drop': 
                     drop = True
                     print "gonna drop 2"
                 else:
                     drop = False
         #print drop
-    def greater_than(ip1,ip2):
+        return drop
+
+    def greater_than(self,ip1,ip2):
+        if ip1 == ip2:
+            return True
         split_ip1 = ip1.split('.')
         split_ip2 = ip2.split('.')
-        for i in range(length(split_ip1)):
-            if(int(split_ip1[i]) < int(split_ip2[i])):
+        for i in range(len(split_ip1)):
+            if(int(split_ip1[i]) > int(split_ip2[i])):
                 return True
-        return False
+            elif(int(split_ip1[i]) < int(split_ip2[i])):
+                return False
 
     def geo_search(self, ip, geo):
        # print ip
         if len(geo) <= 1:
-            print "failed"
-            print "failed"
+            #print "failed"
+            #print "failed"
             return "failed"
-        if(geo[len(geo)/2][0] <= ip and geo[len(geo)/2][1] >= ip):
-            print ip
-            print "found", geo[len(geo)][2]
-            return geo[len(geo)][2]
-        elif geo[len(geo)/2][0] > ip:
+        if self.greater_than(ip, geo[len(geo)/2][0]) and self.greater_than(geo[len(geo)/2][1], ip):
+        #if(geo[len(geo)/2][0] <= ip and geo[len(geo)/2][1] >= ip):
+            #print ip
+            #print geo[len(geo)/2]
+            #print "found", geo[len(geo)/2][2]
+            return geo[len(geo)/2][2]
+        elif self.greater_than(geo[len(geo)/2][0], ip):
+        #elif geo[len(geo)/2][0] > ip:
             #print "left"
-            print geo[len(geo)/2][0],'>', ip
+            #print geo[len(geo)/2][0],'>', ip
             return self.geo_search(ip,geo[0:len(geo)/2])
         else:
             #print "right"
-            print geo[len(geo)/2][0],'<', ip
+            #rint geo[len(geo)/2][0],'<', ip
             return self.geo_search(ip,geo[len(geo)/2:len(geo)])
         
     def ip_conv(self, extIP):
@@ -197,8 +300,8 @@ class Firewall:
         fourth = extIP & 255
         fourth = str(fourth)
         result = first + '.' + second + '.' + third + '.' + fourth
-        print result 
-        print extIP
+        #print result 
+        #print extIP
         return result
 
     # TODO: You can add more methods as you want.
